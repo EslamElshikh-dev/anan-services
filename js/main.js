2138363522ea95b20e1a6da183c7d4140dc25a59
Eslam Eshikh <moqawel1215@gmail.com>
2026-06-17 18:07:30 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
+document.addEventListener('DOMContentLoaded', () => {
 
+    console.log('Anan Services Loaded');
+
+    const links = document.querySelectorAll('a[href^="#"]');
+
+    links.forEach(link => {
+
+        link.addEventListener('click', function(e) {
+
+            const target = document.querySelector(
+                this.getAttribute('href')
+            );
+
+            if (target) {
+
+                e.preventDefault();
+
+                target.scrollIntoView({
+                    behavior: 'smooth'
+                });
+
+            }
+
+        });
+
+    });
+
+});
